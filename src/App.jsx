43f27718ad6d9e8ab555e2c830d78ed1d44a648f
Hovhannes Petrosyan <hovopetrosyan2000@gmail.com>
2023-01-30 12:45:56 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Cart from './components/Cart';
+
+
+const App = () => {
+  return (
+    <div>
+      <Cart/>
+    </div>
+  )
+}
+
+export default App
